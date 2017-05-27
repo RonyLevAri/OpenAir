a77c1590d990a822ded7aef39335d58ea422941f
Rony Lev-Ari <ronylevari@gmail.com>
2017-05-28 01:15:25 +0300

    changed ui react boilerplate to an updated boilerplate, using webpack2

---
+++ openair/ui/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+import '../src/styles/index.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- openair/ui/webpack.config.js
+++ openair/ui/webpack.config.js
@@ -1,65 +1,80 @@
-"use strict";
-var webpack = require('webpack');
-var path = require('path');
-var loaders = require('./webpack.loaders');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var DashboardPlugin = require('webpack-dashboard/plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
 
-const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || "8888";
-
-loaders.push({
-  test: /\.scss$/,
-  loaders: ['style-loader', 'css-loader?importLoaders=1', 'sass-loader'],
-  exclude: ['node_modules']
+const isProductionMode = process.env.NODE_ENV === 'production';
+const cssDev = [{loader: 'style-loader'}, {loader: 'css-loader'},{loader: 'sass-loader'}];
+const cssProd = ExtractTextPlugin.extract({
+    use: [
+        {loader: 'css-loader'},
+        {loader: 'sass-loader'}
+    ],
+    fallback: 'style-loader',
+    publicPath: path.resolve('dist')
 });
+const cssConfig = isProductionMode ? cssProd : cssDev;
 
 module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.jsx', // your app's entry point
-  ],
-  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
-  output: {
-    publicPath: '/',
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    loaders
-  },
-  devServer: {
-    contentBase: "./public",
-    // do not print bundle build stats
-    noInfo: true,
-    // enable HMR
-    hot: true,
-    // embed the webpack-dev-server runtime into the bundle
-    inline: true,
-    // serve index.html in place of 404 responses to allow HTML5 history
-    historyApiFallback: true,
-    port: PORT,
-    host: HOST
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true
-    }),
-    new DashboardPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-			title: 'Open Air',
-      files: {
-        css: ['style.css'],
-        js: [ "bundle.js"],
-      }
-    }),
-  ]
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            path.resolve('src')
+        ],
+        extensions: ['.js', '.jsx', '.css', '.json', '.scss'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: cssConfig
+            },
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                include: [
+                    path.resolve('src')
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg|svg)$/,
+                loader: [
+                    'file-loader?name=[name].[ext]&outputPath=images/',
+                    'image-webpack-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join('dist'),
+        compress: true,
+        hot: true
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'index.bundle.css',
+            disable: !isProductionMode,
+            allChunks: true
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+            title: 'Open Air',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
 };
