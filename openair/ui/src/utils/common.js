e1d52a85bbdc2817312ec2e63d805812b6ada2eb
Rony Lev-Ari <ronylevari@gmail.com>
2017-06-06 17:01:03 +0300

    added try catch to buildStation function

--- openair/ui/src/utils/common.js
+++ openair/ui/src/utils/common.js
@@ -1,13 +1,19 @@
 export function buildStationObj(data) {
     let station = {};
-    station.key = data["pk"];
-    station.id = data["pk"];
-    station.isActive = data["fields"]["is_active"];
-    station.latitude = data["fields"]["latitude"];
-    station.longitude = data["fields"]["longitude"];
-    station.name = data["fields"]["name"];
-    station.isChosen = false;
-    station.isSelectable = data["fields"]["is_active"];
+    try {
+        station.key = data["pk"];
+        station.id = data["pk"];
+        station.isActive = data["fields"]["is_active"];
+        station.latitude = data["fields"]["latitude"];
+        station.longitude = data["fields"]["longitude"];
+        station.name = data["fields"]["name"];
+        station.isChosen = false;
+        station.isSelectable = data["fields"]["is_active"];
+    }
+    catch(err) {
+        station = {key: data, id: data, isActive: false, latitude: 0, longitude: 0, name: "", isChosen: false, isSelectable: false}
+    }
+
     return station;
 }
 
